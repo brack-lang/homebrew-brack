a04ec4b56969a9f79a5b8389f1c9541dfe8736f9
Mutsuha Asada <me@momee.mt>
2024-10-06 10:00:26 +0900

    fix: sha256 hash

--- Formula/brack.rb
+++ Formula/brack.rb
@@ -2,7 +2,7 @@ class Brack < Formula
   desc "A bracket-based lightweight markup language that extends commands with WebAssembly"
   homepage "https://github.com/brack-lang/brack"
   url "https://github.com/brack-lang/brack/releases/tag/v0.0.1"
-  sha256 "350ffa0a895786d83514eebfdd26fb5e8ad44671b46cc479797b4c2277508915"
+  sha256 "08ad2f7bc5b21aa357dcd0ffa40dc0ddf4df91fc8de9969e453ce84abc62f32a"
   license "MIT OR Apache-2.0"
 
   def install
